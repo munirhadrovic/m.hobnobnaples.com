8a5adffba337e7b78cc5c24bb4d56fa92f1978f7
Emir <emiratesbp@gmail.com>
2015-03-30 11:04:56 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<link rel="stylesheet" href="css/jquery.mobile-1.4.5.css">
+		<title>HOBNOB</title>
+		<script src="js/jquery-1.11.2.js"></script>
+		<script>
+		    $(document).on("mobileinit", function(evt) {
+		      // Change some settings
+		    	$.mobile.defaultPageTransition = "flip";
+		    	$.mobile.pageLoadErrorMessage = "Whoops!";
+		    	$.mobile.pageLoadErrorMessageTheme = "b";
+			});
+		</script>
+		<script src="js/jquery.mobile-1.4.5.js"></script>
+		<link rel="stylesheet" href="css/slicknav.css" />
+		<script src="js/jquery.slicknav.js"></script>
+		<script type="text/javascript" src="js/responsiveslides.js"></script>
+		<link rel="stylesheet" href="css/style-mobile.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script>
+			
+			$(function(){
+			   	$( "[data-role='header'], [data-role='footer']" ).toolbar();
+			});	    	
+		   	$(function(){
+				$('#menu').slicknav({
+					prependTo:'#navigation-menu'
+				});
+			});
+			$(function(){
+				$('#menu-contact').slicknav({
+					prependTo:'#navigation-menu-contact'
+				});
+			});
+			$(function(){
+				$('#menu-seo').slicknav({
+					prependTo:'#navigation-menu-seo'
+				});
+			});
+			$(function () {
+
+		      // Slideshow 4
+		      	$("#slider4").responsiveSlides({
+			        auto: true,
+			        pager: false,
+			        nav: true,
+			        speed: 500,
+			        namespace: "callbacks",
+			        before: function () {
+			          $('.events').append("<li>before event fired.</li>");
+			        },
+			        after: function () {
+			          $('.events').append("<li>after event fired.</li>");
+			        }
+		      	});
+
+		    });
+		    $(function () {
+
+		      // Slideshow web
+		      	$("#slider-web").responsiveSlides({
+			        auto: true,
+			        pager: false,
+			        nav: false,
+			        speed: 500,
+			        namespace: "callbacks",
+			        before: function () {
+			          $('.events').append("<li>before event fired.</li>");
+			        },
+			        after: function () {
+			          $('.events').append("<li>after event fired.</li>");
+			        }
+		      	});
+
+		    });	
+    	</script>
+    	
+	</head>
+	<body>
+		
+		<?php 
+			include 'settings.php';
+		?>
+
+		<div id="header-image" data-role="header" data-theme="<?php echo $theme ?>" data-position="fixed">
+		    <h1><a href="#page1"><img src="img/logo.png" alt="LOGO"></a></h1>
+		</div>
+		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
+			<h1>Powered by <a href="http://mobilegrows.com/">MobileGrows</a> | <a href="http://hobnobnaples.com/" rel="external">Desktop Site</a></h1>
+		</div>
+		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
+		  
+		  <div data-role="main" class="ui-content" data-theme="<?php echo $theme ?>">
+		  		<!-- Jssor Slider Begin -->
+			  	<div class="callbacks_container">
+				    <ul class="rslides" id="slider4">
+				      <li>
+				        <img src="img/slider-images/Screenshot_7.png" alt="">
+				        <!-- <p class="caption">This is a caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/slider.png" alt="">
+				        <!-- <p class="caption">This is another caption</p> -->
+				      </li>
+				      <li>
+				        <img src="img/slider-images/slider1.png" alt="">
+				        <!-- <p class="caption">The third caption</p> -->
+				      </li>
+				    </ul>
+			    </div>
+			    <!-- Jssor Slider End -->
+				<fieldset class="ui-grid-a" data-theme="<?php echo $theme ?>">
+					<!-- OVDJE SE UPISUJE BROJ TELEFONA KOJI TREBA POZVATI  -->
+					<div class="ui-block-a"><a class="ui-btn callfind  ui-icon-phone ui-btn-icon-left buttons-radius" href="tel:<?php echo $telephone ?>">CALL US</a></div>
+					<!-- ovdje se upisuje grad i adresa iz koje  -->
+					<div class="ui-block-b" id="findUS">
+						<script>
+							var ua = navigator.userAgent;
+							if(/Android|webOS|Opera Mini/i.test(navigator.userAgent) ) {
+								console.log("Android uslo");
+								var prostordugme=document.getElementById('findUS');
+								var dugme= document.createElement('a');
+								dugme.setAttribute('class', "ui-btn callfind  ui-icon-location ui-btn-icon-right buttons-radius");
+								dugme.setAttribute('href',"geo:0,0?q=<?php echo $adresa; ?>,<?php echo $grad; ?>,<?php echo $skracenica; ?>");
+								dugme.innerHTML="FIND US";
+								prostordugme.appendChild(dugme);
+							}
+							else if(/iPhone|iPad|iPod/i.test(navigator.userAgent)){
+								console.log("Iphone ");
+								var prostordugme=document.getElementById('findUS');
+								var dugme= document.createElement('a');
+								dugme.setAttribute('class', "ui-btn callfind  ui-icon-location ui-btn-icon-right buttons-radius");
+								dugme.setAttribute('href',"http://maps.google.com/?daddr=<?php echo $adresa; ?>,<?php echo $grad; ?>,<?php echo $skracenica; ?> ");
+								dugme.innerHTML="FIND US";
+								prostordugme.appendChild(dugme);
+							}
+							else if (ua.indexOf("BlackBerry") >= 0) {
+
+									console.log("Blakberu je prosao ")
+									var prostordugme=document.getElementById('findUS');
+									var dugme= document.createElement('a');
+									dugme.setAttribute('class', "ui-btn callfind  ui-icon-location ui-btn-icon-right buttons-radius");
+									dugme.setAttribute('href',"javascript:blackberry.launch.newMap({'address':{'address1':'<?php echo $adresa; ?>,<?php echo $grad; ?>,<?php echo $skracenica; ?>'}});");
+									dugme.innerHTML="FIND US";
+									prostordugme.appendChild(dugme);
+									}		
+							else {
+								console.log("nije nigdje uslo default ")
+								var prostordugme=document.getElementById('findUS');
+								var dugme= document.createElement('a');
+								dugme.setAttribute('class', "ui-btn callfind  ui-icon-location ui-btn-icon-right buttons-radius");
+								dugme.setAttribute('href',"geo:0,0?q=<?php echo $adresa; ?>,<?php echo $grad; ?>,<?php echo $skracenica; ?>");
+								dugme.innerHTML="FIND US";
+								prostordugme.appendChild(dugme);
+							}
+
+							
+
+							// if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+							// // some code..
+							// }
+
+						</script>
+						
+						
+
+					</div>	   
+				</fieldset>				
+				
+              <div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-info ui-alt-icon ui-btn-icon-right buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">ABOUT US</a></div>
+				</div>
+              <div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-info ui-alt-icon ui-btn-icon-right buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">MENU</a></div>
+				</div>
+              <div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-info ui-alt-icon ui-btn-icon-right buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">BAR</a></div>
+				</div>
+              <div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-info ui-alt-icon ui-btn-icon-right buttons-radius" href="#page2" data-transition="<?php echo $transitionefect ?>">RESERVATION</a></div>
+				</div>
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<div class="ui-block-a buttons-semir "><a class="ui-btn ui-icon-mail ui-alt-icon ui-btn-icon-right buttons-radius" href="#contactform" data-transition="<?php echo $transitionefect ?>">CONTACT US</a></div>
+				</div>				   
+		  </div>
+
+            <h2>Welcome to HOBNOB</h2><hr>            
+            <p>Hobnob is the brainchild of long time Naples restaurateurs Michael and Lisa Hernandez. Here we provide an eclectic American Style Cuisine menu that is fresh, clean, and simple. Designed with a sexy swagger and a trend setting bar, this is “the place to be”. Just check Opentable.com where we have just been named one of “The 100 Hottest Restaurants in America”</p>
+
+		</div>
+		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+		<!-- page 2 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		<div data-role="page" id="page2" data-theme="<?php echo $theme ?>">
+			<div data-role="main" class="ui-content" data-theme="<?php echo $theme ?>">
+				<div class="ui-grid-solo ui-alt-icon" data-theme="<?php echo $theme ?>">
+					<section id="navigation-menu">
+						<ul id="menu">
+							<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">HOME</a></li>
+							<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">ABOUT US</a></li>
+							<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">MENU</a></li>
+							<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">BAR</a></li>
+							<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">RESERVATION</a></li>
+							<li><a href="#contactform" data-transition="<?php echo $transitionefect ?>">CONTACT US</a></li>
+						</ul>
+					</section>
+
+                    <p>This is only a <b>PREVIEW</b> version of your Mobile Website.</p>
+                    <hr/>
+                    <p>Please contatact our official Sales Representative for <b>Full Functional Version.</b></p>
+
+
+                </div>
+
+			</div>	
+				
+				
+		</div>
+		
+		<!-- page 2 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+		<!-- contact us ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		
+		<div id="contactform" data-role="page" data-theme="<?php echo $theme ?>">
+			<div data-role="main" class="ui-content" >
+				<div class="ui-grid-solo ui-alt-icon" data-theme="<?php echo $theme ?>">
+					<section id="navigation-menu-contact">
+							<ul id="menu-contact">
+                                <li><a href="#page1" data-transition="<?php echo $transitionefect ?>">HOME</a></li>
+								<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">ABOUT US</a></li>
+								<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">MENU</a></li>
+								<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">BAR</a></li>
+								<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">RESERVATION</a></li>
+							</ul>
+					</section>
+					<div>	
+						<form data-parsley-validate method="post" action="mail.php" data-ajax="false">
+							Name: <input type="text" id="naam" name="naam" value="" placeholder="Name" required/><br>
+							E-mail: <input type="email" id="mail" name="mail" value="" placeholder="E-mail" required/><br>
+							Phone Number: <input type="tel" id="telefoon" name="telefoon" value="" placeholder="Phone"/><br>
+							Subject: <input type="text" id="onderwerp" name="onderwerp" value="" placeholder="Subject" required/><br>
+							Message:<br>
+							<textarea style="height:100px;" id="bericht" name="bericht" placeholder="Enter your message here..." required></textarea> <br />
+							<input type="submit" value="Send" name="submit" class="btn" id="input-dugme">
+						</form>
+					</div>
+				</div>
+			</div>
+		
+		</div>
+<!--contact us///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<!-- page seo ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		<!--///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		<div id="seopage" data-role="page" data-theme="<?php echo $theme ?>">
+			<div data-role="main" class="ui-content" >
+				<div class="ui-grid-solo" data-theme="<?php echo $theme ?>">
+					<section id="navigation-menu-seo">
+							<ul id="menu-seo">
+								<li><a href="#page1" data-transition="<?php echo $transitionefect ?>">Home</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Web Design</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Local SE0</a></li>
+								<li><a href="#page2" data-transition="<?php echo $transitionefect ?>">Web Analytics</a></li>
+							</ul>
+					</section>
+					
+				</div>
+			</div>
+		
+		</div>
+<!--page seo///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		<div id="succes" data-role="page" data-theme="<?php echo $theme ?>" data-dialog="true">
+		
+			<div data-role="main" class="ui-content" >	
+			 <p>The contact form was sent succesful!</p>
+			
+			</div>
+		
+		</div>
+<!--page succes ends here///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		<div id="error" data-role="page" data-dialog="true">
+		
+			<div data-role="main" class="ui-content" >	
+				<p>The contact form was sent because of an error!</p>
+			</div>
+		
+		</div>
+<!--erroor ends here////////////////////////////////////////////////////////////////////////////////////////////////////
+	<!-- page 3 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+		<!-- SLIDER ZA MOBILNI SAJT POCINJE OVDJEE -->
+		<!-- it works the same with all jquery version from 1.x to 2.x -->
+	
+		<script type="text/javascript" src="js/responsiveslides.js"></script>    
+	
+	   
+		<!-- Slajder se zavrsava -->
+		<script src="js/easing.js" type="text/javascript"></script>
+		<!-- UItoTop plugin -->
+		<script src="js/jquery.ui.totop.js" type="text/javascript"></script>
+		<!-- Starting the plugin -->
+		<script type="text/javascript">
+			$(document).ready(function() {
+				/*
+				var defaults = {
+		  			containerID: 'toTop', // fading element id
+					containerHoverID: 'toTopHover', // fading element hover id
+					scrollSpeed: 1200,
+					easingType: 'linear' 
+		 		};
+				*/
+				
+				$().UItoTop({ easingType: 'easeOutQuart' });
+				
+			});
+		</script>
+	</body>
+</html>
---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="720 5th Ave. South Unit 101 Naples"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Florida 34102";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2395800070;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="pop"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
